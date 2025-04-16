1bf7892e23f1807b7e2f02ec89069bb9fd5424c8
hamid11392904 <hamedreza1992@gmail.com>
2025-04-16 10:15:03 +0330

    crate url verificode

--- aplication/app/user/urls.py
+++ aplication/app/user/urls.py
@@ -3,6 +3,7 @@ from app.user.views import SendCode, SingIn, VerifiCode, SignupView
 
 urlpatterns = [
     path('send_code/', SendCode.as_view(), name='send_ciode'),
+    path('verify_code/', VerifiCode.as_view(), name='verify'),
 
 
 ]
