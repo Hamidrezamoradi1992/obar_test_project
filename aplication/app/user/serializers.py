62ef22aa0bead58232d5889c8a65e22f1827bed8
hamid11392904 <hamedreza1992@gmail.com>
2025-04-16 10:07:40 +0330

    crate serializers PhoneNumberSerializer,LoginUserSerializer,ValidationCodeSerializers,SignupSerializers

---
+++ aplication/app/user/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+
+from app.core.utils.utils import Utils
+from .models import User
+
+
+class PhoneNumberSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(
+        max_length=11,
+        required=True,
+        help_text=""
+    )
+
+    def validate_phone_number(self, value):
+        if not Utils.vilification_pattern_mobile(value):
+            raise serializers.ValidationError(
+                "شماره تلفن باید با +98 یا 09 شروع شده و دقیقاً ۱۱ رقم داشته باشد.")
+        return value
+
+
+class LoginUserSerializer(PhoneNumberSerializer):
+    password = serializers.CharField(
+        max_length=32,
+        # min_length=8,
+        required=True,
+        help_text="شماره تلفن باید با +98 یا 09 شروع شده و دقیقاً ۱۱ رقم داشته باشد."
+    )
+
+
+class ValidationCode(PhoneNumberSerializer):
+    code = serializers.CharField(
+        max_length=6,
+        # min_length=8,
+        required=True,
+        help_text="شماره تلفن باید با +98 یا 09 شروع شده و دقیقاً ۱۱ رقم داشته باشد."
+    )
+
+
+class SignupSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password', "phone"]
