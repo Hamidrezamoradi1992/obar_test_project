03d08cf8c0453f99f01c56e8820d9842e8f2a1ff
hamid11392904 <hamedreza1992@gmail.com>
2025-04-16 10:20:13 +0330

    crate url singin and singup

--- aplication/app/user/urls.py
+++ aplication/app/user/urls.py
@@ -4,6 +4,7 @@ from app.user.views import SendCode, SingIn, VerifiCode, SignupView
 urlpatterns = [
     path('send_code/', SendCode.as_view(), name='send_ciode'),
     path('verify_code/', VerifiCode.as_view(), name='verify'),
-
+    path('singin/', SingIn.as_view(), name='sign_in'),
+    path('signup/', SignupView.as_view(), name='signup'),
 
 ]
